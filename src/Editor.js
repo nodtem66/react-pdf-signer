ac91d06731eca3c4220921e6144bdc9f24c495b3
Jirawat I <nodtem66@gmail.com>
2020-04-01 13:30:12 +0700

    initial comit

---
+++ src/Editor.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import useEsc from './hook/useEsc'
+
+function Editor(props) {
+    const [mode, setMode] = useState(null)
+    if (mode === 'text' || mode === 'draw') {
+        document.body.style.cursor = 'crosshair'
+    } else {
+        document.body.style.cursor = 'default'
+    }
+    return (
+        <div className="Editor">
+            { mode ?
+            <div className="Editor-item" onClick={() => setMode(null)}>Cancel</div>
+            :
+            <>
+            <div className="Editor-item" onClick={() => setMode('signature')}>Signature</div>
+            <div className="Editor-item" onClick={() => setMode('draw')}>Draw</div>
+            <div className="Editor-item" onClick={() => setMode('Image')}>Image</div>
+            <div className="Editor-item" onClick={() => setMode('text')}>Text</div>
+            </>
+            }
+        </div>
+    )
+}
+
+export default Editor
